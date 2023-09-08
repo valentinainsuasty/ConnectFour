2f89ca7cd4294d694a5d4e4376f8d63251c7e159
Stiven <73106994+stivenrodriguezm@users.noreply.github.com>
2023-09-08 15:28:49 -0500

    Se agrego contador 4

--- Ev.java
+++ Ev.java
@@ -1,62 +1,68 @@
-public class Evaluador {
+public class Ev {
     public static void main(String[] args) {
         int Tamano = 6;
-        char Simbolo_Jugador_1 = '1';
-        char Simbolo_Jugador_2 = '2';
+        char Simbolo_Jugador_1 = 'X';
+        char Simbolo_Jugador_2 = 'Y';
         boolean Turno1 = true;
-        int x = 2;
-        int y = 2;
+        int x = 3;
         char simbolo = Simbolo_Jugador_1;
-        boolean esTriqui = false;
+        boolean ConnectFour = false;
 
         if(!Turno1)
             simbolo = Simbolo_Jugador_2;
-        
+
+        int[] ys = {0, 0, 0, 0, 0, 0};
+
         char[][] matriz = {
                 {' ', ' ', ' ', ' ', ' ', ' '},
                 {' ', ' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', ' ', 'x', ' ', ' '},
-                {' ', 'y', 'x', 'x', ' ', ' '},
-                {'y', 'x', 'x', 'y', ' ', ' '},
-                {'x', 'y', 'y', 'x', ' ', ' '}
+                {' ', ' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', 'X', ' ', ' '},
+                {' ', 'X', 'Y', 'Y', ' ', ' '},
+                {'X', 'Y', 'Y', 'X', ' ', ' '}
         };
+
+        //int[] ys = {1, 2, 3, 4, 0, 0};
+        int y = Tamano - ys[x];
+
+
 
         int contador = 0;
 
         // Verifico la línea horizontal:
-        for(int i = 0; i < matriz.length; i++){
+        for(int i = 0; i < 4; i++){
             if(matriz[y][i] == simbolo){
                 contador += 1;
             }
         }
-        esTriqui = (contador == 3);
+        ConnectFour = (contador == 4);
         // Verifico la línea vertical:
-        if(!esTriqui){
+        if(!ConnectFour){
             contador = 0;
-            for(int i = 0; i < matriz.length; i++){
+            for(int i = 0; i < 4; i++){
                 if(matriz[i][x] == simbolo){
                     contador += 1;
                 }
             }
-            esTriqui = (contador == 3);
+            ConnectFour = (contador == 4);
             // Verifico la diagonal principal:
-            if(x == y && !esTriqui){
+            if(x == y && !ConnectFour){
                 contador = 0;
-                for(int i = 0; i < matriz.length; i++){
+                for(int i = 0; i < 4; i++){
                     if(matriz[i][i] == simbolo){
                         contador += 1;
                     }
                 }
-                esTriqui = (contador == 3);
+                ConnectFour = (contador == 4);
                 // Verifico la diagonal invertida:
-                if(x + y == Tamano - 1 && !esTriqui){
+                if(x + y == Tamano - 1 && !ConnectFour){
                     contador = 0;
-                    for(int i = 0; i < matriz.length; i++){
+                    for(int i = 0; i < 4; i++){
                         if(matriz[i][Tamano-1-i] == simbolo){
                             contador += 1;
                         }
                     }
-                    esTriqui = (contador == 3);
+                    ConnectFour = (contador == 4);
                 }
             }
         }
@@ -68,9 +74,9 @@ public class Evaluador {
             }
             System.out.println();
         }
-        if(esTriqui)
-            System.out.println("Es Triqui");
+        if(ConnectFour)
+            System.out.println("Es ConnectFour");
         else
-            System.out.println("NO es Triqui");
+            System.out.println("No es ConnectFour");
     }
 }
---
+++ Ta.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Ta {
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+
+        // Parámetros del juego:
+        int TAMANO = 6;
+        char Simbolo_Jugador_1 = 'X';
+        char Simbolo_Jugador_2 = 'Y';
+        boolean Turno1 = true;
+
+        // Tablero:
+        char[][] matriz = new char[TAMANO][TAMANO];
+
+        // Imprimir matriz:
+        for(int i=0; i<matriz.length; i++){
+            for(int j=0; j<matriz[i].length; j++){
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        // Reglas del juego:
+        while(true){
+            char simbolo = Simbolo_Jugador_1;
+            if(!Turno1)
+                simbolo = Simbolo_Jugador_2;
+            System.out.println("Ingresa tu posición (X)");
+            int x = lector.nextInt();
+
+            matriz[y][x] = simbolo;
+
+            // Imprimir matriz:
+            for(int i=0; i<matriz.length; i++){
+                for(int j=0; j<matriz[i].length; j++){
+                    System.out.print(matriz[i][j] + " ");
+                }
+                System.out.println();
+            }
+            Turno1 = !Turno1;
+        }
+    }
+}
